6330599c70d36eccd5cee2cd3b5f5fca56cdc786
shivnayan <nayan54451@gmail.com>
2026-08-01 11:51:47 +0530

    initial commit

---
+++ index.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std; 
+
+void greetings() { 
+    cout<<"Hello, welcome to the C++20 project!"<<endl;
+}
+
+int main() { 
+    
+    greetings(); 
+    return 0; 
+
+}
