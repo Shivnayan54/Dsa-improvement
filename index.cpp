ee43466e51b7acced289aba91b4034d5aed78c24
shivnayan <nayan54451@gmail.com>
2026-08-06 10:50:22 +0530

    print reverse of an array using function

--- index.cpp
+++ index.cpp
@@ -1,25 +1,37 @@
-#include<iostream>
+#include<iostream> 
 using namespace std; 
 
-int linearSearch(int arr[] , int n , int target){ 
+void reverseArray(int arr[] , int n){ 
 
-    for (int i = 0; i< n ;i++){ 
-        
-        if (arr[i] == target){ 
-            return i; 
-        }
-    }
-    return -1; 
+    int left = 0; 
+    int right = n-1; 
+
+   while(left < right){
+
+        int temp = arr[left]; 
+        arr[left] = arr[right]; 
+        arr[right] = temp; 
+
+        left++; 
+        right--; 
+    } 
 }
 
-int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int n = 5;
-    int target = 30;
+void printArray(int arr[] , int n)
+{
+    for(int i = 0; i < n; i++){
+        cout<<arr[i] << " "; 
+    }
+}
 
-    int index = linearSearch(arr, n, target);
+int main()
+{
+    int arr[] = {10 ,20 ,30 ,40 ,50}; 
+    int n = 5; 
 
-    cout << "Index = " << index;
+    reverseArray(arr, n); 
 
-    return 0;
+    printArray(arr, n); 
+
+    return 0; 
 }
