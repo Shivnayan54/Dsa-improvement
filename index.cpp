90ae94bf3be41868469e35d716c21bc8fd2e145a
shivnayan <nayan54451@gmail.com>
2026-08-07 09:29:18 +0530

    Second Largest using function

--- index.cpp
+++ index.cpp
@@ -1,26 +1,32 @@
 #include<iostream>
 using namespace std; 
 
-bool isSorted(int arr[] , int n){
-    for(int i =0; i<n ;i++){ 
-        if(arr[i] > arr[i + 1]){
-            return false; 
+// Scecond Largest Element in an array 
+
+int SecondLargest(int arr[] , int n){
+
+    int largest = arr[0]; 
+    int second = -1; 
+
+    for(int i=1; i<n ;i++){ 
+
+        if(arr[i] > largest){
+            second = largest; 
+            largest = arr[i]; 
+        }
+        else if(arr[i] > second && arr[i] != largest){ 
+            second = arr[i];
         }
     }
+    return second; 
+} 
+  
+int main(){
+    int arr[] = {10 ,20 ,30 ,40 , 50}; 
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-    return true ;
-}
+    int result = SecondLargest(arr , n);
 
-int main() 
-{ 
-    int arr[] = {1 ,2 ,3 ,4 ,5}; 
-    int n = 5; 
-
-    if(isSorted(arr, n)){
-        cout << "Array is Sorted"; 
-    }else
-    {
-        cout << "Array is Not Sorted"; 
-    }
+    cout<< "The Second Largest Element in the array is: " << result << endl;
     return 0; 
 }
