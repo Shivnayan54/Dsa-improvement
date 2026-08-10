27ab8e831b8c37704aaca59364342fa9a8648dc0
shivnayan <nayan54451@gmail.com>
2026-08-10 11:42:10 +0530

    String intial commit

--- index.cpp
+++ index.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
-using namespace std;
+#include<string>
+using namespace std; 
 
-void binToDec(int binNum) {
-    int n = binNum; 
-    int decNum = 0; 
-    int pow = 1; 
+void reverseString(string &s)
+{
+    int left = 0; 
+    int right = s.length() - 1; 
 
-    while(n > 0) {
-        int lasDig = n % 10; 
-        decNum += lasDig * pow;
-        pow *= 2;
-        n /= 10;
+    while(left<right){
+        char temp = s[left]; 
+        s[left] = s[right]; 
+        s[right] = temp; 
+
+        left++; 
+        right--; 
     }
-
-    cout << "Decimal equivalent of " << binNum << " is: " << decNum << endl;
 }
 
-int main() {
-    binToDec(101);
+int main() { 
+    string s = "hello"; 
+
+    reverseString(s); 
+    cout << s; 
+
     return 0; 
 }
